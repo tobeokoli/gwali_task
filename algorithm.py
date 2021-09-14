ede8196b6da59882bb60efc549f76b965a9256bc
aggr3550r <ucheviquetor@gmail.com>
2021-09-14 15:26:24 +0100

    made changes to geography and fine arts

--- algorithm.py
+++ algorithm.py
@@ -1,8 +1,9 @@
 import json
+import os
 
 final_result = []
 list_of_courses = []
-with open('new_strings.txt') as f:
+with open('raw_strings.txt') as f:
     lines = f.readlines()
 for line in lines:
     course = line.split()
@@ -11,13 +12,16 @@ for course in list_of_courses:
     if not course:
         continue
     course.pop()
-    print(course)
+    # print(course)
     dept_prefix = course[0]
     course_number = course[1]
     course_title = " ".join(course[2:]).upper()
     course_data = {'department_prefix': dept_prefix, 'course_number': course_number, 'course_title': course_title}
     final_result.append(course_data)
+file1 = open("new_string.txt", "w+")
+file1.write(json.dumps(final_result, indent = 4))
 
-print(final_result)
-print(json.dumps(final_result, indent = 4))
+# print(final_result)
+# print(json.dumps(final_result, indent = 4))
+
 
