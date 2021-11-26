a543c5e4ac563052cf2d72900708587298cfcb59
tobeokoli <61335010+tobeokoli@users.noreply.github.com>
2021-11-26 05:27:00 +0100

    all departments now in json format in json_departments folder

--- algorithm.py
+++ algorithm.py
@@ -2,8 +2,10 @@ import json
 
 final_result = []
 list_of_courses = []
-with open('raw_strings.txt') as f:
+course_name = 'social_work'
+with open(f'{course_name}.txt') as f:
     lines = f.readlines()
+f.close()
 for line in lines:
     course = line.split()
     list_of_courses.append(course)
@@ -11,12 +13,15 @@ for course in list_of_courses:
     if not course:
         continue
     course.pop()
-    dept_prefix = course[0]
+    # print(course)
+    dept_prefix = course[0].upper()
     course_number = course[1]
     course_title = " ".join(course[2:]).upper()
     course_data = {'department_prefix': dept_prefix, 'course_number': course_number, 'course_title': course_title}
     final_result.append(course_data)
+file1 = open(f"{course_name}.json", "w+")
+file1.write(json.dumps(final_result, indent = 4))
+file1.close()
 
 print(final_result)
 print(json.dumps(final_result, indent = 4))
-
